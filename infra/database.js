4cefa373709bf616f5ae8d083e5fe01c23d15728
Krisofferson Marini <60137002+ksmarini@users.noreply.github.com>
2023-11-18 15:01:14 +0000

    Resolvido problema da variável do banco

--- infra/database.js
+++ infra/database.js
@@ -4,8 +4,8 @@ async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();

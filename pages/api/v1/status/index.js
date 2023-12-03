7aa59234a613bb38f6efbd41b63b73997c67d4c3
Krisofferson Marini <60137002+ksmarini@users.noreply.github.com>
2023-12-03 17:20:31 +0000

    finalizado status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,20 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updateAt = new Date().toISOString();
+
+  const versaoDB = await database.query("SELECT version()");
+  const maxConnDB = await database.query("SHOW max_connections;");
+  const usedConnections = await database.query(
+    "SELECT datname, numbackends FROM pg_stat_database;",
+  );
+
+  response.status(200).json({
+    update_at: updateAt,
+    db_version: versaoDB.rows[0].version,
+    max_db_conn: maxConnDB.rows[0].max_connections,
+    used_conn: usedConnections.rows[2].numbackends,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.db_version).toMatch(/PostgreSQL/);
+  expect(typeof responseBody.used_conn).toBe("number");
+  expect(typeof parseInt(responseBody.max_db_conn)).toBe("number");
+  //console.log("Meu teste: ", responseBody);
 });

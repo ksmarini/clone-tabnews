2c5bcd0c76ee5cf5683b2de7f628d86d20d3dd88
Krisofferson Marini <60137002+ksmarini@users.noreply.github.com>
2023-12-07 22:38:20 +0000

    updated return endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,23 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updateAt = new Date().toISOString();
 
-  const versaoDB = await database.query("SELECT version()");
-  const maxConnDB = await database.query("SHOW max_connections;");
-  const usedConnections = await database.query(
-    "SELECT datname, numbackends FROM pg_stat_database;",
-  );
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue =
+    await databaseVersionResult.rows[0].server_version;
 
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
   response.status(200).json({
     update_at: updateAt,
-    db_version: versaoDB.rows[0].version,
-    max_db_conn: maxConnDB.rows[0].max_connections,
-    used_conn: usedConnections.rows[2].numbackends,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,13 +3,10 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.db_version).toMatch(/PostgreSQL/);
-  expect(typeof responseBody.used_conn).toBe("number");
-  expect(typeof parseInt(responseBody.max_db_conn)).toBe("number");
-  //console.log("Meu teste: ", responseBody);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
